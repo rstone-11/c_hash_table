3b561348fe0331027d12b4c01780419730db5e91
rstone-11 <95187360+rstone-11@users.noreply.github.com>
2024-08-07 21:55:26 -0700

    handle hash collisions

--- src/hash_table.c
+++ src/hash_table.c
@@ -3,6 +3,9 @@
 #include <math.h>
 #include "hash_table.h"
 
+#define HT_PRIME_1 151
+#define HT_PRIME_2 163
+
 
 static ht_item* ht_new_item(const char* k, const char* v){
     ht_item* i = malloc(sizeof(ht_item));
@@ -26,6 +29,12 @@ static int ht_hash(const char* s, const int a, const int m){
         hash = hash % m;
     }
     return (int)hash;
+}
+
+static int ht_get_hash(const char* s, const int num_buckets, const int attempt){
+    const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
+    const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
+    return (hash_a + (attempt * (hash_b+1))) % num_buckets;
 }
 
 ht_hash_table* ht_new(){
